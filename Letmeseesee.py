ee94dfc8f5096d88a3ce5716a26411429ade3688
Hao093 <394937800@qq.com>
2025-06-26 10:25:07 +0800

    dada

--- Letmeseesee.py
+++ Letmeseesee.py
@@ -4,18 +4,8 @@ def fibonacci(a,b):
 def getadd(c,d):
     return c+d
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-fibonacci(9,9)
-=======
-fibonacci(1,232)
->>>>>>> branch333
-=======
-fibonacci(19,29)
->>>>>>> branch111
-=======
-fibonacci(12,121)
->>>>>>> branch222
+
+fibonacci(91,91)
+
 e = getadd(10,55)
 print(r"我得到的结果是\n",e)
